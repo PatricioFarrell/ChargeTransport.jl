a21b6204cb5e964a5675a1790599dd6fe410f5e7
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2020-10-10 22:22:04 +0200

    set_pdelib_constants()
    Function to modify constants such that solutions can be tested against pdelib.

--- src/ct_constants.jl
+++ src/ct_constants.jl
@@ -9,4 +9,13 @@ mₑ                 = 9.1093837015e-31        # kg           --- electron rest 
 q                  = 1.602176634e-19         # C            --- elementary charge
 ε0                 = 8.8541878176e-12        # (A*s)/ (V*m) --- absolute dielectric permittivity of classical vacuum
 
+
+# Set this as temporary values from pdelib, figure out the recent SI stuff (Unitful.jl ?
+function set_pdelib_constants()
+    global kB=1.3806503e-23
+    global q=1.602176462e-19
+    global ε0=8.85418781762039e-12
+    global eV=q*V
+end
+
 export kB, Planck_constant, mₑ, q, ε0
