de43a0599926849e49973d4333c95dcb197d94c0
Patricio Farrell <patricio.farrell@wias-berlin.de>
2020-11-04 17:47:04 +0100

    Added first try with Unitful/PhysicalConstants (as a comment). Problem: Units need to be erased stiil.

--- src/ct_constants.jl
+++ src/ct_constants.jl
@@ -3,6 +3,19 @@
     Famous constants in SI units
 """
 
+### Below is a first try with Unitful/PhysicalConstants. 
+### Problem: Units need to be erased, see Quire #109 in ChargeTransportInSolids
+
+# using Unitful
+# using PhysicalConstants.CODATA2018
+
+# kB                 = BoltzmannConstant                # JK^{-1}      --- Boltzmann constant
+# Planck_constant    = PlanckConstant                   # Js           --- Planck constant
+# mₑ                 = ElectronMass                     # kg           --- electron rest mass
+# q                  = ElementaryCharge                 # C            --- elementary charge
+# ε0                 = VacuumElectricPermittivity       # (A*s)/ (V*m) --- absolute dielectric permittivity of classical vacuum
+
+
 kB                 = 1.38064852e-23          # JK^{-1}      --- Boltzmann constant
 Planck_constant    = 6.62607015e-34          # Js           --- Planck constant
 mₑ                 = 9.1093837015e-31        # kg           --- electron rest mass
@@ -12,10 +25,10 @@ q                  = 1.602176634e-19         # C            --- elementary charg
 
 # Set this as temporary values from pdelib, figure out the recent SI stuff (Unitful.jl ?
 function set_pdelib_constants()
-    global kB=1.3806503e-23
-    global q=1.602176462e-19
-    global ε0=8.85418781762039e-12
-    global eV=q*V
+    global kB = 1.3806503e-23
+    global q  = 1.602176462e-19
+    global ε0 = 8.85418781762039e-12
+    global eV = q * V
 end
 
 export kB, Planck_constant, mₑ, q, ε0
