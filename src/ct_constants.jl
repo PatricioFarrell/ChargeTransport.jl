449c4572a4735f709d64a3013b2e56c8e9d7ae29
Dilara Abdel <abdel@wias-berlin.de>
2021-02-10 15:18:23 +0100

    add TeSCA constants in function

--- src/ct_constants.jl
+++ src/ct_constants.jl
@@ -16,11 +16,11 @@
 # ε0                 = VacuumElectricPermittivity       # (A*s)/ (V*m) --- absolute dielectric permittivity of classical vacuum
 
 
-kB                 = 1.38064852e-23          # JK^{-1}      --- Boltzmann constant # TeSCA: 1.380662e-23#
+kB                 = 1.38064852e-23          # JK^{-1}      --- Boltzmann constant
 Planck_constant    = 6.62607015e-34          # Js           --- Planck constant
 mₑ                 = 9.1093837015e-31        # kg           --- electron rest mass
-q                  = 1.602176634e-19         # C            --- elementary charge #TeSCA: 1.6021e-19#
-ε0                 = 8.8541878176e-12        # (A*s)/ (V*m) --- absolute dielectric permittivity of classical vacuum #TeSCA: 8.85419e-12#
+q                  = 1.602176634e-19         # C            --- elementary charge
+ε0                 = 8.8541878176e-12        # (A*s)/ (V*m) --- absolute dielectric permittivity of classical vacuum
 
 
 # Set this as temporary values from pdelib, figure out the recent SI stuff (Unitful.jl ?
@@ -28,6 +28,13 @@ function set_pdelib_constants()
     global kB = 1.3806503e-23
     global q  = 1.602176462e-19
     global ε0 = 8.85418781762039e-12
+    global eV = q * V
+end
+
+function set_TeSCA_constants()
+    global kB = 1.380662e-23
+    global q  = 1.6021e-19
+    global ε0 = 8.85419e-12
     global eV = q * V
 end
 
