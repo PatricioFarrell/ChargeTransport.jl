1b9275e10b3b076ae1d16a3062755a08f5ba3769
Dilara Abdel <abdel@wias-berlin.de>
2021-08-18 18:26:05 +0200

    adjusted docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,34 +2,6 @@ using Documenter
 using ChargeTransportInSolids
 using Literate
 
-# following function was just copied from VoronoiFVM
-# Replace SOURCE_URL marker with github url of source
-#
-function replace_source_url(input,source_url)
-    lines_in = collect(eachline(IOBuffer(input)))
-    lines_out=IOBuffer()
-    for line in lines_in
-        println(lines_out,replace(line,"SOURCE_URL" => source_url))
-    end
-    return String(take!(lines_out))
-end
-
-
-example_jl_dir = joinpath(@__DIR__,"..","examples")
-example_md_dir  = joinpath(@__DIR__,"src","examples")
-
-for example_source in readdir(example_jl_dir) base,ext=splitext(example_source)
-    if ext==".jl"
-        source_url="https://github.com/PatricioFarrell/ChargeTransportInSolids.jl/tree/master/examples"*example_source
-        preprocess(buffer)=replace_source_url(buffer,source_url)
-        Literate.markdown(joinpath(@__DIR__,"..","examples",example_source),
-                          example_md_dir,
-                          documenter=false,
-                          info=false,
-                          preprocess=preprocess)
-    end
-end
-generated_examples=vcat(["runexamples.md"],joinpath.("examples",readdir(example_md_dir)))
 
 makedocs(sitename ="ChargeTransportInSolids.jl",
          modules  = [ChargeTransportInSolids],
@@ -37,10 +9,13 @@ makedocs(sitename ="ChargeTransportInSolids.jl",
          doctest  = false,
          authors  = "D. Abdel, P. Farrell, J. Fuhrmann",
          repo     = "https://github.com/PatricioFarrell/ChargeTransportInSolids.jl",
-         pages    = [ "Introducing the Model" =>  "general.md",
-                      "About the discrete System" =>  "system.md",
-                      "Plotting Functions" => "plot.md",
-                      "Examples" => generated_examples ]
+         pages    = [ "ChargeTransportInSolids.jl -- A drift-diffusion solver" =>  "general.md",
+                      "Mathematical Description of the Problem" => "backgroundinfo.md",
+                      "Numerical Strategy" =>  "system.md",
+                      "Some Applications" => ["examples/GaAs.md", "examples/PSC.md"],
+                      ##
+                      "Plotting Functions" => "plot.md"
+                      ]
          )
 
 # deploydocs(
